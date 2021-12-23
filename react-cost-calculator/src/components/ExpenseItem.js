0a8532c34bf9f2a967baba65e80e52326485dca8
NishiKant <007nishi@gmail.com>
2021-12-23 17:05:48 +0530

    Expense Item component creation

---
+++ react-cost-calculator/src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem(){
+
+    return (
+    <div>
+        <div>
+            March 28th 2021
+        </div>
+        <div>
+            <div>
+                <h2>car Insurence</h2>
+            </div>
+            <div>
+                $294.67
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
